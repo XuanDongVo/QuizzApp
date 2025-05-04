cdb7d68345233507188de9bac602f4ea126bf3f5
XuanDong <22130043@st.hcmuaf.edu.vn>
2025-05-04 10:07:32 +0700

    upate (env): env production and dev

--- backend/Quizz/app.js
+++ backend/Quizz/app.js
@@ -4,37 +4,35 @@ const cors = require("cors");
 const app = express();
 const { CONNECT_DB } = require("./src/config/mongoConfig");
 
-const START_SERVER = () => {
-  // Middleware
-  app.use(express.json());
-  app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  }));
+// Middleware
+app.use(express.json());
+app.use(cors({
+  origin: process.env.BUILD_MODE === "production"
+    ? process.env.CLIENT_URL
+    : "http://localhost:3000",
+  credentials: true,
+}));
 
-  // Import routes
-  // const topicRoute = require("./src/routes/topic");
-  const questionRoute = require("./src/routes/question");
-  const openAiRoute = require("./src/routes/openAi");
-  const quizzRoute = require("./src/routes/quizz");
+// Import routes
+const questionRoute = require("./src/routes/question");
+const openAiRoute = require("./src/routes/openAi");
+const quizzRoute = require("./src/routes/quizz");
 
-  // Sử dụng routes
-  // app.use("/topic", topicRoute);
-  app.use("/question", questionRoute);
-  app.use("/openai", openAiRoute);
-  app.use("/quizz", quizzRoute);
+// Sử dụng routes
+app.use("/question", questionRoute);
+app.use("/openai", openAiRoute);
+app.use("/quizz", quizzRoute);
 
-  // Start server
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-};
+// Kết nối cơ sở dữ liệu và khởi động server
+const PORT = process.env.PORT || 3000;
 
 CONNECT_DB()
-  .then(() => { console.log("Connected to database") })
-  .then(() => { START_SERVER(); })
+  .then(() => {
+    console.log("Connected to database");
+    app.listen(PORT, () => {
+      console.log(`Server is running on ${process.env.BUILD_MODE === "production" ? "Production" : "Development"} mode at http://localhost:${PORT}`);
+    });
+  })
   .catch((error) => {
     console.log("Error connecting to database", error);
-  }
-  );
+  });
