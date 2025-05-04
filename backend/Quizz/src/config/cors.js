7af6eeee96312622f1f8a5b345d5e650c494ca0e
XuanDong <22130043@st.hcmuaf.edu.vn>
2025-05-04 23:46:33 +0700

    allow all origin

--- backend/Quizz/src/config/cors.js
+++ backend/Quizz/src/config/cors.js
@@ -1,21 +1,32 @@
 
-WHITELIST_DOMAINS = ["http://localhost:3000", process.env.CLIENT_URL]
+// WHITELIST_DOMAINS = ["http://localhost:3000", process.env.CLIENT_URL]
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (!origin && process.env.BUILD_MODE === 'dev') {
+//             return callback(null, true)
+//         }
+//         if (WHITELIST_DOMAINS.includes(origin)) {
+//             return callback(null, true)
+//         }
+//         return callback(new Error('Not allowed by CORS'))
+//     },
+//     optionsSuccessStatus: 200,
+
+//     credentials: true
+// }
+
+
+// module.exports = {
+//     corsOptions
+// };
+
+
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin && process.env.BUILD_MODE === 'dev') {
-            return callback(null, true)
-        }
-        if (WHITELIST_DOMAINS.includes(origin)) {
-            return callback(null, true)
-        }
-        return callback(new Error('Not allowed by CORS'))
-    },
+    origin: true,
+    credentials: true,
     optionsSuccessStatus: 200,
-
-    credentials: true
-}
-
+};
 
 module.exports = {
-    corsOptions
+    corsOptions,
 };
