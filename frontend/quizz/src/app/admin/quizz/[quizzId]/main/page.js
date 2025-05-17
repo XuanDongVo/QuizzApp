c41306e8fb44bbbfee4b5194858e027e43927fb9
nghia22130183 <22130183@st.hcmuaf.edu.vn>
2025-05-17 15:04:02 +0700

    fix

--- frontend/quizz/src/app/admin/quizz/[quizzId]/main/page.js
+++ frontend/quizz/src/app/admin/quizz/[quizzId]/main/page.js
@@ -1,28 +1,10 @@
-import { useCreatedQuizzContext } from '@/context/createdQuizzContext';
-import Link from 'next/link';
+
+import QuizzMain from "@/components/quizzCreated/QuizzMain";
+import { use } from 'react'
 
 export default function Page({ params }) {
-    const { quizzId } = params;
-    const { quizz, setQuizz } = useCreatedQuizzContext();
-
-    const handleDelete = (id) => {
-        const updated = quizz.questions.filter(q => q.id !== id);
-        setQuizz({ ...quizz, questions: updated });
-    }
-
+    const { quizzId } = use(params);
     return (
-        <div>
-            <h1>Danh sách câu hỏi</h1>
-            <Link href={`/admin/quizz/${quizzId}/created`}>➕ Thêm câu hỏi</Link>
-            <ul>
-                {quizz.questions.map(q => (
-                    <li key={q.id}>
-                        {q.title}
-                        <Link href={`/admin/quizz/${quizzId}/edit/${q.id}`}>✏️</Link>
-                        <button onClick={() => handleDelete(q.id)}>🗑️</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <QuizzMain quizzId={quizzId} />
     )
 }
